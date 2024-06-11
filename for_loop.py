7fab19b90f46960ff370a68ac7a3a81904fe47b3
hernan-vela <hernan.velamar@gmail.com>
2024-06-11 16:12:04 +1000

    Practicing with exercises. Some python file might be modified, but original content has been commented out so it can be restored

--- for_loop.py
+++ for_loop.py
@@ -1,17 +1,17 @@
-# Range arguments (start = 0, stop, step = 1) - This are the default value is nothing is specified
+# Range arguments (start = 0, stop, step = 1) - This are the default value if nothing is specified
 # if you use range(10), the program will print the numbers from 0 to 9.
 
 # print(range(5, 10, 1))
 
-# For loop in a range
-for i in range(10, 5, -2): 
-    print(i)
+# # For loop in a range
+# for i in range(10, 5, -2): 
+#     print(i)
 
 # For loop in a list
-fruits = ["Apple", "Banana", "Cherry"]
+fruits = ["Apple", "Banana", "Guava", "Papaya", "Cherry", "Pineapple", "Dragon fruit", "Coconut", "Mandarine"]
 
-# for each in fruits:
-#     print(each)
+# for i in fruits:
+#     print(i)
 
 # for each in "CoderAcademy":
 #     print(each)
--- guessing_game.py
+++ guessing_game.py
@@ -1,24 +1,43 @@
 # Guessing Game
 
-guess_num = 5
+prize_num = 14
 
 user_guess = None
+
+while user_guess != prize_num:
+    user_guess = int(input("Enter a number between 1 and 20 win the prize: "))
+    if user_guess > prize_num:
+        print("You are over. Dial down")
+    elif user_guess < prize_num:
+        print("Don't be timid. Go higher!")
+    else:
+        print("Winner, winner, chicken dinner!")
+
+
+
+
+
+
+
+# guess_num = 5
+
+# user_guess = None
+
+# # while user_guess != guess_num:
+# #     user_guess = int(input("What's your guess (between 1 and 10)? "))
+
+# # print("Correct!")
+
+# #Make it better
+# #Give hints
 
 # while user_guess != guess_num:
 #     user_guess = int(input("What's your guess (between 1 and 10)? "))
 
-# print("Correct!")
-
-#Make it better
-#Give hints
-
-while user_guess != guess_num:
-    user_guess = int(input("What's your guess (between 1 and 10)? "))
-
-    if user_guess < guess_num:
-        print("Too low!")
-    elif user_guess > guess_num:
-        print("Too high!")
-    else:
-        print("Correct!")
+#     if user_guess < guess_num:
+#         print("Too low!")
+#     elif user_guess > guess_num:
+#         print("Too high!")
+#     else:
+#         print("Correct!")
 
