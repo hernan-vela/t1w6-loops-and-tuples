a4678c848ae7f4e31194727392286a02018ec554
hernan-vela <hernan.velamar@gmail.com>
2024-06-24 15:30:28 +1000

    Update of calculator.py and list_example.py

--- calculator.py
+++ calculator.py
@@ -1,23 +1,47 @@
-# Simple calculator using if-else statements
+# # Simple calculator using if-else statements
 
-# Get user inputs
-num1 = float(input("Enter first number: "))
-operation = input("Enter operation (+, -, *, /): ")
-num2 = float(input("Enter secnond number: "))
+# # Get user inputs
+# num1 = float(input("Enter first number: "))
+# operation = input("Enter operation (+, -, *, /): ")
+# num2 = float(input("Enter secnond number: "))
 
-# # Perform calculation using if-else statments
-if operation == '+':
+# # # Perform calculation using if-else statments
+# if operation == '+':
+#     result = num1 + num2
+# elif operation == '-':
+#     result = num1 - num2
+# elif operation == '*':
+#     result = num1 * num2
+# elif operation == '/':
+#     if num2 != 0:
+#         result = num1 / num2
+#     else:
+#         result = "Error! Division by Zero"
+# else:
+#     result = "Error! Invalid Operation"
+
+# print(f"The result is: {result}")
+
+# Next, a simple calculator using if-else statements
+
+num1 = float(input("Enter your first number: "))
+operation = input("What operation you want to perform (+, -, *, /): ")
+num2 = float(input("Enter your second number: "))
+
+
+if operation == "+":
     result = num1 + num2
-elif operation == '-':
+elif operation == "-":
     result = num1 - num2
-elif operation == '*':
+elif operation == "*":
     result = num1 * num2
-elif operation == '/':
-    if num2 != 0:
-        result = num1 / num2
-    else:
+elif operation == "/":
+    if num2 == 0:
         result = "Error! Division by Zero"
+    else:
+        result = num1 / num2
 else:
-    result = "Error! Invalid Operation"
+    result = "Error! What the hell are you doing?"
 
-print(f"The result is: {result}")
+print(f"The result of this operation is: {result}")
+
--- list_example.py
+++ list_example.py
@@ -1,35 +1,36 @@
-# country1 = "Australia"
-# country2 = "New Zealand"
-# country3 = "USA"
-# country4 = "England"
+# # country1 = "Australia"
+# # country2 = "New Zealand"
+# # country3 = "USA"
+# # country4 = "England"
 
-# print(country1)
-# print(country2)
-# print(country3)
-# print(country4)
+# # print(country1)
+# # print(country2)
+# # print(country3)
+# # print(country4)
 
-# countries = ["Australia", "New Zealand", "USA", "England"]
+# # countries = ["Australia", "New Zealand", "USA", "England"]
 
-# print(countries)
+# # print(countries)
 
-# print(countries)
+# # print(countries)
 
-# countries.remove("USA")
+# # countries.remove("USA")
 
-# print(countries)
+# # print(countries)
 
-# len(countries)
+# # len(countries)
 
-exercises = ["pull-ups", "push-ups", "squat", "skin the cat", "romanian squat", "pistol squats"]
-print(exercises)
+# exercises = ["pull-ups", "push-ups", "squat", "skin the cat", "romanian squat", "pistol squats"]
+# print(exercises)
 
-# append command add another value to the list in the last position
-exercises.append("handstand")
-print(exercises)
+# # append command add another value to the list in the last position
+# exercises.append("handstand")
+# # print(exercises)
 
-# remove command extract this given value from the list if it is present. If not it returns 'Value Error'
-exercises.remove("romanian squat")
-print(exercises)
+# # remove command extract this given value from the list if it is present. If not it returns 'Value Error'
+# exercises.remove("romanian squat")
+# # print(exercises)
 
-# returns the number of items in the list
-print(len(exercises))
+# # returns the number of items in the list
+# print(len(exercises))
+
